9d91d4331562d097de16a5aa62e22063debbbf41
hks0827 <p90p90p09@gmail.com>
2025-06-01 21:54:10 +0900

    first commit

---
+++ src/main/java/com/blc/blc_backend/BlcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blc.blc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blc/blc_backend/BlcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blc.blc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
