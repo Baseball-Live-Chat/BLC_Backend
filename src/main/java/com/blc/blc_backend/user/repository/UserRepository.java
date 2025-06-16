f701901f10dc16e276eb7aae5815371b48603abd
Spinichi <spinichi@naver.com>
2025-06-16 23:11:46 +0900

    chore: users 패키지명 변경

--- src/main/java/com/blc/blc_backend/user/repository/UserRepository.java
+++ src/main/java/com/blc/blc_backend/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.blc.blc_backend.users.repository;
+package com.blc.blc_backend.user.repository;
 
-import com.blc.blc_backend.users.entity.User;
+import com.blc.blc_backend.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
