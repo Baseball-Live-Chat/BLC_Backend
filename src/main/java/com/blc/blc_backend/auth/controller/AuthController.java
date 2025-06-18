1a242f5214064673bd4a9d47ea39a49b561af6b5
Spinichi <spinichi@naver.com>
2025-06-19 02:01:40 +0900

    feat[#10]: logout 기능 구현

--- src/main/java/com/blc/blc_backend/auth/controller/AuthController.java
+++ src/main/java/com/blc/blc_backend/auth/controller/AuthController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
-
     private final AuthenticationManager authenticationManager;
 
     /**
